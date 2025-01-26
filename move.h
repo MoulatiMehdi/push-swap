beeae93a4d49c8786bd917dceab72131aeaac173
Mehdi_Moulati <moulati.mehdi@gmail.com>
2025-01-27 00:03:20 +0100

    fix merge conflict betweem move and bistack

--- move.h
+++ move.h
@@ -1,22 +1,24 @@
 #ifndef MOVE_H
 
-#define MOVE_H
+# define MOVE_H
+
+# include "stack.h"
+# include <unistd.h>
 
 typedef enum e_move
 {
-    MOV_PA = 0, // 
-    MOV_PB = 1, //
-    MOV_SA , //
-    MOV_SB , //
-    MOV_SS , //
-    MOV_RA ,
-    MOV_RB ,
-    MOV_RR ,
-    MOV_RRA,
-    MOV_RRB,
-    MOV_RRR,
+	MOV_PA = 0,
+	MOV_PB = 1,
+	MOV_SA,
+	MOV_SB,
+	MOV_SS,
+	MOV_RA,
+	MOV_RB,
+	MOV_RR,
+	MOV_RRA,
+	MOV_RRB,
+	MOV_RRR
+}		t_move;
 
-} t_move;
-
-
-#endif 
+void	t_move_optimize(t_stack *head);
+#endif

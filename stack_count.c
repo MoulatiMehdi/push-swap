8a9075909700c041f12540592e42e2dcd9e28e9f
MoulatiMehdi <moulati.mehdi@gmail.com>
2025-01-28 23:14:15 +0100

    fix norminette issues and add functions

---
+++ stack_count.c
@@ -0,0 +1,65 @@
+#include "stack.h"
+
+long	t_stack_size(t_stack *head)
+{
+	long	i;
+
+	i = 0;
+	while (head)
+	{
+		i++;
+		head = head->next;
+	}
+	return (i);
+}
+
+long	t_stack_num_at(t_stack *head, long idx)
+{
+	long	i;
+
+	i = 0;
+	while (i < idx)
+	{
+		head = head->next;
+		i++;
+	}
+	return (head->num);
+}
+
+long	t_stack_max(t_stack *head)
+{
+	int		target;
+	long	i;
+	t_stack	*tmp;
+
+	tmp = head;
+	i = -1;
+	target = tmp->num;
+	while (tmp)
+	{
+		if (tmp->num > target)
+			target = tmp->num;
+		tmp = tmp->next;
+		i++;
+	}
+	return (target);
+}
+
+long	t_stack_min(t_stack *head)
+{
+	int		target;
+	long	i;
+	t_stack	*tmp;
+
+	tmp = head;
+	i = -1;
+	target = tmp->num;
+	while (tmp)
+	{
+		if (tmp->num < target)
+			target = tmp->num;
+		tmp = tmp->next;
+		i++;
+	}
+	return (target);
+}
